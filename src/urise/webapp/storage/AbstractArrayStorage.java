7b8016840cf71a0d58c7db6d717120a56dbadc62
KoloDYS <kolody-serge1@yandex.ru>
2023-05-15 23:16:04 +0300

    hw 3

--- src/urise/webapp/storage/AbstractArrayStorage.java
+++ src/urise/webapp/storage/AbstractArrayStorage.java
@@ -9,7 +9,7 @@ public abstract class AbstractArrayStorage implements Storage {
     protected int size;
     protected final Resume[] storage = new Resume[CAPACITY];
 
-    public void clear() {
+    public final void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
@@ -17,11 +17,11 @@ public abstract class AbstractArrayStorage implements Storage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
+    public final Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public void save(Resume r) {
+    public final void save(Resume r) {
         int index = getIndex(r.getUuid());
         if (size == CAPACITY) {
             System.out.println("Список резюме переполнен");
@@ -33,7 +33,7 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public Resume get(String uuid) {
+    public final Resume get(String uuid) {
         int index = getIndex(uuid);
         if (!isExist(index)) {
             System.out.println("Резюме " + uuid + " не найдено");
@@ -43,11 +43,11 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public int size() {
+    public final int size() {
         return size;
     }
 
-    public void update(Resume resume) {
+    public final void update(Resume resume) {
         int index = getIndex(resume.getUuid());
         if (!isExist(index)) {
             System.out.println("Резюме " + resume.getUuid() + " для обновления не найдено");
@@ -56,7 +56,7 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public void delete(String uuid) {
+    public final void delete(String uuid) {
         int index = getIndex(uuid);
         if (!isExist(index)) {
             System.out.println("Резюме " + uuid + " для удаления не найдено");
