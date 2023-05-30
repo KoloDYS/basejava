6279c0f1bc95b1130e7e2c1851424e0fe4e48c9f
KoloDYS <kolody-serge1@yandex.ru>
2023-05-31 01:54:09 +0300

    hw_5, fix mapStorage

--- src/urise/webapp/storage/MapStorage.java
+++ src/urise/webapp/storage/MapStorage.java
@@ -7,50 +7,50 @@ import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
 
-    private final Map<Integer, Resume> mapStorage = new HashMap<Integer, Resume>();
+    private final Map<String, Resume> storage = new HashMap<String, Resume>();
 
     @Override
     protected Resume getResume(Object searchKey) {
-        return mapStorage.get((int) searchKey);
+        return storage.get((String) searchKey);
     }
 
     @Override
     protected void removeResume(Object searchKey) {
-        mapStorage.remove((int) searchKey);
+        storage.remove((String) searchKey);
     }
 
     @Override
     protected void updateResume(Resume resume, Object searchKey) {
-        mapStorage.put((int) searchKey, resume);
+        storage.put((String) searchKey, resume);
     }
 
     @Override
     protected void saveResume(Resume resume, Object searchKey) {
-        mapStorage.put((int) searchKey, resume);
+        storage.put((String) searchKey, resume);
     }
 
     @Override
     protected boolean isExist(Object searchKey) {
-        return searchKey != null && mapStorage.containsKey((Integer) searchKey);
+        return storage.containsKey((String) searchKey);
     }
 
     @Override
     protected Object getSearchKey(String uuid) {
-        return ((uuid.indexOf("uuid")) == 0) ? Integer.parseInt(uuid.split("uuid")[1]) : null;
+        return uuid;
     }
 
     @Override
     public void clear() {
-        mapStorage.clear();
+        storage.clear();
     }
 
     @Override
     public Resume[] getAll() {
-        return mapStorage.values().toArray(new Resume[0]);
+        return storage.values().toArray(new Resume[0]);
     }
 
     @Override
     public int size() {
-        return mapStorage.size();
+        return storage.size();
     }
 }
